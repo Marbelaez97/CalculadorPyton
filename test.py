441816f833ce61479afd2b6c2f26b05bb3fdd968
Marbelaez97 <45182560+Marbelaez97@users.noreply.github.com>
2019-11-26 14:13:26 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from Operaciones import Operaciones
 class calculadoraTest(unittest.TestCase):
 
     def test_suma(self):
-        self.assertEqual(Operaciones.suma(1,2),5)
+        self.assertEqual(Operaciones.suma(1,2),3)
 
     def test_resta(self):
         self.assertEqual(Operaciones.resta(2,1),1)
